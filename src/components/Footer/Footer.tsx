d5080832b8fc98962170052978f1d7b639762dcf
Shreyas Gavhalkar <shreyasgavhalkar10@gmail.com>
2023-05-01 13:20:36 +0530

    initial commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+		<p>Made using github actions</p>
+        </footer>
+    )
+}
+
+export default Footer;
